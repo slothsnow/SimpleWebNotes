a7b95a4d5c005aabe8a1897128e35a93c46a0050
slothsnow <143102784+slothsnow@users.noreply.github.com>
2025-06-11 18:00:03 +0200

    Activate (for testing) HTTP Basic Auth

--- src/main/java/slothsnow/simpleWebNotes/SecurityConfig.java
+++ src/main/java/slothsnow/simpleWebNotes/SecurityConfig.java
@@ -2,7 +2,9 @@ package slothsnow.simpleWebNotes;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,6 +13,12 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(request -> request
+                                        .requestMatchers("/note/**")
+                                        .authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
